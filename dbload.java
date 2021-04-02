5612bee043227e2019a27c1c13bf77d599f34654
s3707294 <49424596+s3707294@users.noreply.github.com>
2021-04-03 10:04:49 +1100

    Add files via upload
    Started program

---
+++ dbload.java
@@ -0,0 +1,30 @@
+/*
+Erin Sutton, s3707294
+COSC2406 Assignment 1, Task 3
+*/
+
+//Imports
+import com.copencsv.CSVreader;
+
+import java.io.FileReader;
+import java.util.Scanner;
+
+//Some important values
+pageTotal = 0;
+pageSize = 0;
+currentPageFree = 0;
+
+recordTotal = 0;
+recordPage = 0;
+
+public static void main(String[] args) {
+
+  //Get the data file path
+  Scanner sc = new Scanner(System.in);
+  System.out.println('Enter path to data :');
+  String dataPath = sc.nextLine();
+
+
+
+
+}
