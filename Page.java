57f99c02e66a980a0a2aa734e1366e27d1f2bfd5
s3707294 <49424596+s3707294@users.noreply.github.com>
2021-04-05 11:16:10 +1000

    Add files via upload

--- Page.java
+++ Page.java
@@ -1,19 +1,59 @@
-package dbload;
-
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.LinkedList;
 
 public class Page {
 	
-	//Create page using linked list
+	  //Create page using linked list
 	  LinkedList<Object[]> entry = new LinkedList<Object[]>();
+	 
+	  //Number of columns per entry
+	  int colNum = 9;
 
-	  //consruct a new page
+	  //Construct a new page
 	  public Page(){
 	  }
 
 	  //Add an entry to the page
 	  public void addEntry(Object[] e){
 	    entry.addLast(e);
+	  }
+	  
+	  //Write out page to the heap
+	  public void writeToHeap(FileWriter writeHeap) throws IOException {
+		  
+		  //loop through entry and add to heap file
+		  for(Object[] instance : entry) {
+			  
+			  //Write each column to file
+			  //ID column
+			  writeHeap.write((int) instance[0]);
+			  writeHeap.write(",");
+			  //SDT_Name column
+			  writeHeap.write((String) instance[1]);
+			  writeHeap.write(",");
+			  //Year column
+			  writeHeap.write((int) instance[2]);
+			  writeHeap.write(",");
+			  //Month column
+			  writeHeap.write((String) instance[3]);
+			  writeHeap.write(",");
+			  //Mdate column
+			  writeHeap.write((int) instance[4]);
+			  writeHeap.write(",");
+			  //Day column
+			  writeHeap.write((String) instance[5]);
+			  writeHeap.write(",");
+			  //Time column
+			  writeHeap.write((int) instance[6]);
+			  writeHeap.write(",");
+			  //Sensor_Name column
+			  writeHeap.write((String) instance[7]);
+			  writeHeap.write(",");
+			  //Hourly_Counts column
+			  writeHeap.write((int) instance[8]);
+			  writeHeap.write("\n");			  
+		  }
 	  }
 
 }
--- dbload.java
+++ dbload.java
@@ -3,8 +3,6 @@ Erin Sutton, s3707294
 COSC2406 Assignment 1, Task 3
 */
 
-package dbload;
-
 //Imports
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -16,33 +14,37 @@ import java.util.*;
 public class dbload {
 	
 	//Some important values
-	  int pageTotal = 0;
+	  static int pageTotal = 0;
 	  static int pageSize = 0;
-	  int currentPageFree = 0;
+	  static int currentPageFree = 0;
 
-	  int recordTotal = 0;
-	  int recordPage = 0;
+	  static int recordTotal = 0;
+	  static int recordPage = 0;
 
 	public static void main(String[] args) throws IOException {
 		//Set the pageSize and create file name using this size
 	    pageSize = Integer.parseInt(args[1]);
-	    String heapfileName = "heap." + String.valueOf(pageSize);
-	    String heapfilePageNum = heapfileName + ".pages.txt";
+	    currentPageFree = pageSize;
+	    String heapFileName = "heap." + String.valueOf(pageSize);
+	    String heapfilePageNum = heapFileName + ".pages.txt";
 	    
 	    //Start the timing the loading of data into heap file
 	    long start = System.nanoTime();
 
 	    //open file that will record number of records in each page
-	    FileWriter writePage = new FileWriter(heapfilePageNum);
+	    FileWriter writePage = new FileWriter(heapfilePageNum, true);
 	    
-	  //Create the heap
+	    //Create the heap and create first page
 	    LinkedList<Page> heap = new LinkedList<Page>();
+	    Page currPage = new Page();
+	    pageTotal++;
 
 	    //Open csv data
 	    try {
 	    	BufferedReader br = new BufferedReader(new FileReader(args[2]));
 	    	
 	    	String line = "";
+	    	
 	    		    	
 	    	//Go through each row
 	    	while((line = br.readLine()) != null) {
@@ -50,63 +52,135 @@ public class dbload {
 	    		//Split using the delimiter
 	    		String[] instance = line.split(",");
 	    		
-	    		//Convert row into types and count number of bytes
-	    		int byteSize = 0;		
-	    		Object[] dataEntry =  new Object[8];
-	    		convertRow(dataEntry, instance, byteSize);
+	    		//Convert row into types and count number of bytes	    				
+	    		Object[] dataEntry =  new Object[9];
+	    		dataEntry = convertRow(dataEntry, instance);
+	    		int byteSize = countBytes(dataEntry);	    		
 	    		
+	    		//Check if their is room on page and act accordingly
+	    		currentPageFree -= byteSize;
 	    		
-	    		
+	    		//If room on current Page
+	    		if(currentPageFree >= 0){
+	    			//add row to current page
+	    			currPage.addEntry(dataEntry);
+	    			//Increment count of records on page
+	    			recordPage++;	    			
+	    		}else { //If not enough room on current page
+	    			
+	    			//write number of records on previous page out
+	    			writePage.write(String.valueOf(recordPage));
+	    			writePage.write("\n");
+	    			//Update counts
+	    			recordTotal += recordPage;
+	    			recordPage = 0;
+	    			pageTotal++;
+	    			currentPageFree = pageSize;
+	    			//Add page to the heap
+	    			heap.addLast(currPage);
+	    			//Create new page
+	    			currPage = new Page();
+	    			//add row to new page
+	    			currPage.addEntry(dataEntry);
+	    			//Increment count of records on page
+	    			recordPage++;
+	    			//Update currentPageFree
+	    			currentPageFree -= byteSize;	    	
+	    		}   		
+	    		    		
 	    	}
+	    	//close the data file
+	    	br.close();
+	    	//close the file detailing number of records per page
+	    	writePage.close();
 	    }catch(IOException e) {
 	    	e.printStackTrace();
 	    }
-
 	    
-
+	    //Write the heap to file
+	    writeHeap(heap, heapFileName);
 	    
+	    //end the timing	    
+	    long end = System.nanoTime();
+	    
+	    //Print out Number of records loaded
+	    System.out.println("Number of records loaded : " + recordTotal);
+	    //Print out number of pages used
+	    System.out.println("Number of pages used : " + pageTotal);
+	    //Print out number of miliseconds to create the heap file
+	    System.out.println("Number of milliseconds to create heap file : " + ((end - start)/ 1000000));   
+    
 	}
 	
-	//Converts the row to a correct types entry and counts the number of bytes used
-	 public static void convertRow(Object[] entry, String[] row, int rowByteSize){
+	//Converts the row to a correct types entry
+	 public static Object[] convertRow(Object[] entry, String[] row){		 
 
 	    //Convert ID
-	    entry[0] = Integer.valueOf(row[0]);
-	    rowByteSize += 4;
+	    entry[0] = Integer.valueOf(row[0]);	    
 
 	    //Create SDT_Name
 	    String sdt_name = row[7] + row[1];
 	    entry[1] = sdt_name;
-	    rowByteSize += sdt_name.getBytes().length;
-	    
+	    	    
 	    //Convert Year
 	    entry[2] = Integer.valueOf(row[2]);
-	    rowByteSize += 4;
-	    
+	    	    
 	    //Input Month
-	    entry[3] = row[3];
-	    rowByteSize += row[3].getBytes().length;
+	    entry[3] = row[3];	    
 	    
 	    //Convert Mdate
-	    entry[4] = Integer.valueOf(row[4]);
-	    rowByteSize += 4;
+	    entry[4] = Integer.valueOf(row[4]);	    
 	    
 	    //Input Day
-	    entry[5] = row[5];
-	    rowByteSize += row[5].getBytes().length;
+	    entry[5] = row[5];	    
 	    
 	    //Convert Time
-	    entry[6] = Integer.valueOf(row[6]);
-	    rowByteSize += 4;
+	    entry[6] = Integer.valueOf(row[6]);	    
 	    
 	    //Input Sensor Name
-	    entry[7] = row[8];
-	    rowByteSize += row[8].getBytes().length;   
+	    entry[7] = row[8];	       
 	    
+	    //Convert Hourly_Counts
+	    entry[8] = Integer.valueOf(row[9]);	
+	    
+	    return entry;
 	  }
 	 
-	 public void load(List<String[]> row, LinkedList<Page> heap){
-
-	  }
-
+	 //Counts the number of bytes per record
+	 public static int countBytes(Object[] dataEntry) {
+		 int bytes = 0;
+		 
+		 //There are 5 columns that are integers, integers take 4 bytes
+		 bytes += 5*4;
+		 
+		 //Get bytes for the string columns
+		 //SDT_Name
+		 bytes += ((String) dataEntry[1]).getBytes().length;
+		 //Month
+		 bytes += ((String) dataEntry[3]).getBytes().length;
+		 //Day
+		 bytes += ((String) dataEntry[5]).getBytes().length;
+		 //Sensor_Name
+		 bytes += ((String) dataEntry[7]).getBytes().length;
+		 
+		 return bytes;
+	 }
+	 
+	 //Writes each page into the heap file
+	 public static void writeHeap(LinkedList<Page> heap, String fileName) {
+		 
+		 //open the file
+		 try { 
+			 FileWriter writeHeap = new FileWriter(fileName, true);
+			 
+			 //loop through each page of the heap
+			 for(Page p : heap) {
+				 p.writeToHeap(writeHeap);
+			 }
+			 writeHeap.close();
+		 }catch(IOException e) {
+			 e.printStackTrace();
+		 }
+	 }
+	 
 }
